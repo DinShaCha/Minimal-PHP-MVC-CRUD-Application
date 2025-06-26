26bac7aa601232e86b527ed8aac986fa9cbe24d7
Dinushka Chandrasena <43511312+DinShaCha@users.noreply.github.com>
2025-06-26 10:18:24 +0530

    Add files via upload

---
+++ products_202506261012.sql
@@ -0,0 +1,4 @@
+INSERT INTO basic_crud_1.products (title,description,image,price,created_date) VALUES
+	 ('Wireless Headphones','High-quality over-ear wireless headphones with noise cancellation.','images/PJHp6PgW/headphones.jpg',149.99,'2025-06-23 05:47:06'),
+	 ('Smart LED Bulb','Color-changing smart LED bulb compatible with Alexa and Google Assistant.','images/FMsMMqCe/LED.jpg',24.50,'2025-06-23 05:47:26'),
+	 ('Wireless Earbuds','High-quality wireless earbuds with noise cancellation and 20-hour battery life.','images/cNuBF3Qq/earbuds.jpg',49.99,'2025-06-23 05:47:45');
